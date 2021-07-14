0a682a235c5ac92597311569aaa0c05b9c79c4d8
Ocasu <84593248+Ocasu@users.noreply.github.com>
2021-07-13 23:12:23 -0700

    Update Main.java

--- src/main/java/botMain/Main.java
+++ src/main/java/botMain/Main.java
@@ -8,7 +8,7 @@ import net.dv8tion.jda.api.entities.VoiceChannel;
 
 import javax.security.auth.login.LoginException;
 
-public class Main {
+public static class Main {
     private static JDA bot;
     public static boolean bracketInProgress = false;
     public static Guild guild;
